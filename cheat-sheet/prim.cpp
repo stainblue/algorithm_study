89f83f0c84951cd7d0d63c3c51491de8e5254a24
Doin Cha <chadoin.dev@gmail.com>
2021-01-05 18:59:35 +0900

    Update prim.cpp
    주석 간격조정

--- cheat-sheet/prim.cpp
+++ cheat-sheet/prim.cpp
@@ -6,22 +6,22 @@ using namespace std;
 
 #define MAX_VERTICES 1000
 
-int adj[MAX_VERTICES + 1][MAX_VERTICES + 1];		// 인접행렬
-int isVisit[MAX_VERTICES + 1];								// 이 vertex가  MST = (Y, F)의 Y(vertices 집합)에 포함됐는지 여부
-int dist[MAX_VERTICES + 1];									// 이 vertex와 Y와의 최소거리. (이미 포함된 경우 0)
-int nVerteices;														// vertex 개수
+int adj[MAX_VERTICES + 1][MAX_VERTICES + 1];	// 인접행렬
+int isVisit[MAX_VERTICES + 1];					// 이 vertex가  MST = (Y, F)의 Y(vertices 집합)에 포함됐는지 여부
+int dist[MAX_VERTICES + 1];						// 이 vertex와 Y와의 최소거리. (이미 포함된 경우 0)
+int nVerteices;									// vertex 개수
 
 void prim() {
-	dist[1] = 0;														// V1부터 시작
-	isVisit[1] = true;												// V1이 Y에 포함됨
+	dist[1] = 0;										// V1부터 시작
+	isVisit[1] = true;									// V1이 Y에 포함됨
 	for (int i = 2; i <= nVerteices; i++) {
-		if (dist[i] > adj[1][i]) dist[i] = adj[1][i];			// V1과 인접해있는 vertex들의 distance를 adj[1][vertexIndex]로 갱신
+		if (dist[i] > adj[1][i]) dist[i] = adj[1][i];	// V1과 인접해있는 vertex들의 distance를 adj[1][vertexIndex]로 갱신
 	}
 
-	for (int i = 0; i < nVerteices - 1; i++) {					// V1은 이미 Y에 포함됐으므로 N - 1번 반복
+	for (int i = 0; i < nVerteices - 1; i++) {			// V1은 이미 Y에 포함됐으므로 N - 1번 반복
 		// G = (V, E)의 V - Y 중에서 Y와 가장 가까운 vertex 찾기
-		int minDist = INT_MAX;									// 가장 가까운 vertex와의 거리
-		int vnear;													// 가장 가까운 vertex의 index
+		int minDist = INT_MAX;							// 가장 가까운 vertex와의 거리
+		int vnear;										// 가장 가까운 vertex의 index
 		for (int j = 1; j <= nVerteices; j++) {
 			if (!isVisit[j] && dist[j] < minDist) {
 				minDist = dist[j];
