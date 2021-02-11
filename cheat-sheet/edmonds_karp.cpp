bdcf98a80be4e40587a12d9a7bfaf09ba6185765
Doin Cha <stainblue1129@gmail.com>
2021-02-11 23:13:02 +0900

    Create edmonds_karp.cpp

---
+++ cheat-sheet/edmonds_karp.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std; 
+
+const int MAXV = 1000;
+
+class maximumFlow {
+private:
+	struct edge {
+		int from, to, capacity, flow;
+		edge* reverse;
+
+		edge(int f, int t, int c) : from(f), to(t), capacity(c), flow(0) {}
+
+		int residual() { return capacity - flow; }
+		void addFlow(int newFlow) {
+			flow += newFlow;
+			reverse->flow -= newFlow;
+		}
+	};
+
+	vector<edge*> adj[MAXV + 1];
+public:
+	int nVertices, nEdges;
+	int source, sink;
+	int maxFlow = 0;
+
+	void addEdge(int from, int to, int capacity, bool directed = true) {
+		edge* e = new edge(from, to, capacity);
+		edge* er = new edge(to, from, directed ? 0 : capacity);
+		e->reverse = er;
+		er->reverse = e;
+		adj[from].push_back(e);
+		adj[to].push_back(er);
+	}
+
+
+	// bfs
+	bool isAugmentingPathExist(int prev[], edge** backEdge) {
+		for (int i = 0; i < MAXV + 1; i++) {
+			prev[i] = -1;
+			backEdge[i] = NULL;
+		}
+		queue<int> q;
+		q.push(source);
+		while (!q.empty() && prev[sink] == -1) {
+			int cur = q.front(); q.pop();
+			for (auto e : adj[cur]) {
+				int next = e->to;
+				if (prev[next] == -1 && e->residual() > 0) {
+					prev[next] = cur;
+					backEdge[next] = e;
+					q.push(next);
+				}
+			}
+		}
+		if (prev[sink] == -1) return false;
+		return true;
+	}
+
+	// Edmonds-Karp
+	int findMaxFlow() {
+		int prev[MAXV + 1];
+		edge* backEdge[MAXV + 1] = { 0, };
+		while (isAugmentingPathExist(prev, backEdge)) {
+			int newFlow = 1000;
+
+			for (int i = sink; i != source; i = prev[i]) {
+				newFlow = min(newFlow, backEdge[i]->residual());
+			}
+
+			for (int i = sink; i != source; i = prev[i]) {
+				backEdge[i]->addFlow(newFlow);
+			}
+
+			maxFlow += newFlow;
+		}
+		return maxFlow;
+	}
+};
